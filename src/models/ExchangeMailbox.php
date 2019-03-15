ea0acb2b692b8517dde22550ab5cf2cc294e78a0
andres3210 <andres3210@gmail.com>
2019-03-15 13:22:23 -0400

    Bug fix - Folder structure sync - long id comparison

--- src/models/ExchangeMailbox.php
+++ src/models/ExchangeMailbox.php
@@ -25,12 +25,22 @@ class ExchangeMailbox extends Model
 
         $folders = $exchange->listFolders();
 
-        foreach( $folders AS $folder )
-            ExchangeFolder::firstOrCreate(['item_id' => $folder->id],[
-                'exchange_mailbox_id' => $this->id,
-                'name' => $folder->name,
-                'parent_id' => $folder->ParentFolderId
-            ]);
+        foreach( $folders AS $folder ){
+            $existing = ExchangeFolder::where([
+                'item_id' => $folder->id,
+                'exchange_mailbox_id' => $this->id
+            ])->first();
+
+            // re-compare ID as mysql is limited to 171 bytes and EWS ID is longer
+            if( !$existing || $existing->item_id != $folder->id )
+                ExchangeFolder::create([
+                    'item_id' => $folder->id,
+                    'exchange_mailbox_id' => $this->id,
+                    'name' => $folder->name,
+                    'parent_id' => $folder->ParentFolderId
+                ]);
+        }
+
 
         return $folders;
     }
